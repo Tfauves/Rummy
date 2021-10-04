6e4b6c40bc609ae513d17c5e429353f6a833ced3
Tfauves <tfauvelle@gmail.com>
2021-10-04 15:41:07 -0400

    standard deck class.

---
+++ src/com/company/deck/StandardDeck.java
@@ -0,0 +1,13 @@
+package com.company.deck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StandardDeck {
+    private final List<Card> cards = new ArrayList<>();
+    public final String[] SUITS = {"♦", "♠", "♣", "♥"
+    };
+    public final int[] FACE_VALUE = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+    };
+
+}
