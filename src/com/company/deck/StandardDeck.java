727609f7c4cadb96cd55dbab51c3fd29d6e36888
Tfauves <tfauvelle@gmail.com>
2021-10-04 15:44:21 -0400

    deck methods declared.

--- src/com/company/deck/StandardDeck.java
+++ src/com/company/deck/StandardDeck.java
@@ -7,7 +7,17 @@ public class StandardDeck {
     private final List<Card> cards = new ArrayList<>();
     public final String[] SUITS = {"♦", "♠", "♣", "♥"
     };
-    public final int[] FACE_VALUE = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+    public final int[] VALUE = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
     };
+
+    public StandardDeck() {
+        for (String suit : SUITS) {
+            for (int value : VALUE) {
+                cards.add(new Card(suit, value));
+            }
+
+        }
+    }
+
 
 }
