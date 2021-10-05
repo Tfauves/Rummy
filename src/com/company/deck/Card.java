63605e516fe192df8fdb764a300de6cbc5e536a9
Tfauves <tfauvelle@gmail.com>
2021-10-04 20:41:30 -0400

    defined card display.

--- src/com/company/deck/Card.java
+++ src/com/company/deck/Card.java
@@ -18,7 +18,18 @@ public class Card implements Deck{
     }
 
     public String display() {
-
+        String outPut = "";
+//        if (isFaceDown) {
+//            return "<*>";
+//        }
+        switch (getRANK()) {
+            case 1 -> outPut = "ACE";
+            case 11 -> outPut = "JACK";
+            case 12 -> outPut = "QUEEN";
+            case 13 -> outPut = "KING";
+            default -> outPut = getRANK() == 10 ? Integer.toString(getRANK()) : " " + getRANK();
+        };
+        return outPut;
     }
 
     @Override
