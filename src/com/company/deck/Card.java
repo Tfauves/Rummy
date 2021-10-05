97f56f89c1aef41c9988409176b5849d34fd994c
Tfauves <tfauvelle@gmail.com>
2021-10-04 20:46:58 -0400

    refactor Card class.

--- src/com/company/deck/Card.java
+++ src/com/company/deck/Card.java
@@ -1,27 +1,28 @@
 package com.company.deck;
 
 public class Card implements Deck{
-    private final String SUIT;
-    private final int RANK;
+    private final String suit;
+    private final int rank;
+    private boolean isFaceDown = true;
 
-    public Card(String SUIT, int RANK) {
-        this.SUIT = SUIT;
-        this.RANK = RANK;
+    public Card(String suit, int rank) {
+        this.suit = suit;
+        this.rank = rank;
     }
 
     public int getRANK() {
-        return RANK;
+        return rank;
     }
 
     public String toString() {
-        return RANK + " " + SUIT;
+        return rank + " " + suit;
     }
 
     public String display() {
         String outPut = "";
-//        if (isFaceDown) {
-//            return "<*>";
-//        }
+        if (isFaceDown) {
+            return "<*>";
+        }
         switch (getRANK()) {
             case 1 -> outPut = "ACE";
             case 11 -> outPut = "JACK";
@@ -29,7 +30,7 @@ public class Card implements Deck{
             case 13 -> outPut = "KING";
             default -> outPut = getRANK() == 10 ? Integer.toString(getRANK()) : " " + getRANK();
         };
-        return outPut;
+        return outPut + " " + suit;
     }
 
     @Override
@@ -38,4 +39,10 @@ public class Card implements Deck{
     }
 
     public void shuffle() {}
+
+    public void flip() {
+        isFaceDown = !isFaceDown;
+    }
+
+    public boolean getIsFaceDown() {return isFaceDown;}
 }
