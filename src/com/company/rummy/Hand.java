3b60e9ecc0627b380d54136c4edb25d8c1a110ba
Tfauves <tfauvelle@gmail.com>
2021-10-08 21:22:51 -0400

    hand and addCard method.

--- src/com/company/rummy/Hand.java
+++ src/com/company/rummy/Hand.java
@@ -9,4 +9,14 @@ public class Hand {
     private List<PlayingCard> cards = new ArrayList<>();
     private Actor player;
 
+    public Hand(Actor player) {
+        this.player = player;
+    }
+
+    public void addCard(PlayingCard card) {
+        cards.add(card);
+    }
+
+
+
 }
