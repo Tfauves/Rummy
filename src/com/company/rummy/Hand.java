60e9f0d50aab69a30c3514b5bc7819c4f6ff205d
Tfauves <tfauvelle@gmail.com>
2021-10-08 21:19:28 -0400

    fields declared in hand class

--- src/com/company/rummy/Hand.java
+++ src/com/company/rummy/Hand.java
@@ -1,4 +1,12 @@
 package com.company.rummy;
 
+import com.company.deck.PlayingCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
+    private List<PlayingCard> cards = new ArrayList<>();
+    private Actor player;
+
 }
