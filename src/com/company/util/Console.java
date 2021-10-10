cdef3cfa773d2a4de7da7c6940579618fb8ee47a
Tfauves <tfauvelle@gmail.com>
2021-10-09 21:03:52 -0400

    getInt method.

--- src/com/company/util/Console.java
+++ src/com/company/util/Console.java
@@ -1,5 +1,25 @@
 package com.company.util;
 
+import java.util.Scanner;
+
 public class Console {
+    private static final Scanner scanner = new Scanner(System.in);
+
+
+    public static int getInt(String prompt, int min, int max, String errorMsg) {
+        int option = min -1;
+        do {
+            System.out.println(prompt);
+            String input = scanner.nextLine();
+            try {
+                option = Integer.parseInt(input);
+
+            }catch (NumberFormatException err) {
+                System.out.println(errorMsg);
+            }
+
+        } while (option < min || option > max);
+        return option;
+    }
 
 }
