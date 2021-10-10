333decd02bda0ea7b51975b1a36fa6b500d2f784
Tfauves <tfauvelle@gmail.com>
2021-10-09 20:43:09 -0400

    display table defined.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,18 @@ import com.company.deck.Deck;
 import com.company.deck.PlayingCard;
 import com.company.deck.StandardDeck;
 import com.company.rummy.Hand;
+import com.company.rummy.Table;
 
 public class Main {
 
     public static void main(String[] args) {
-        Deck rumDeck = new StandardDeck();
-        Hand myHand = new Hand(new Player("travis"));
-        myHand.addCard(rumDeck.draw());
-        System.out.println(myHand.displayHand());
+//        Deck rumDeck = new StandardDeck();
+//        Hand myHand = new Hand(new Player("travis"));
+//        myHand.addCard(rumDeck.draw());
+//        System.out.println(myHand.displayHand());
+        Table rummy = new Table();
+        rummy.deal();
+        rummy.displayTable();
 
     }
 }
--- src/com/company/rummy/Table.java
+++ src/com/company/rummy/Table.java
@@ -11,11 +11,19 @@ public class Table {
     private List<Hand> hands = new ArrayList<>();
     private Deck deck = new StandardDeck();
 
-    private void deal() {
+    public void deal() {
         for (int count = 0; count < 10; count++) {
             for (Hand player : hands) {
                 player.addCard(deck.draw());
             }
         }
+    }
+
+    public void displayTable() {
+        StringBuilder outPut = new StringBuilder();
+        for (Hand player : hands) {
+            System.out.println(outPut.append(player.displayHand()));
+        }
+        System.out.println(outPut);
     }
 }
