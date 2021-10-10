46fce1ab5dc93f20bf05ad2f17f429ee71c7a266
Tfauves <tfauvelle@gmail.com>
2021-10-09 22:40:25 -0400

    defined methods int discarded deck.

--- src/com/company/deck/DiscardDeck.java
+++ src/com/company/deck/DiscardDeck.java
@@ -1,13 +1,24 @@
 package com.company.deck;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DiscardDeck implements Deck {
+    private List<Card> discardedCards = new ArrayList<>();
+
     @Override
     public Card draw() {
-        return null;
+        return discardedCards.remove(0);
     }
 
     @Override
     public void shuffle() {
+        Collections.shuffle(discardedCards);
 
+    }
+
+    public List getDiscardedCards() {
+        return discardedCards;
     }
 }
