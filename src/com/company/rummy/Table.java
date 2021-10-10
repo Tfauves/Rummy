25972fec7bf337a2f83e2361ae3a44e3141ec7ac
Tfauves <tfauvelle@gmail.com>
2021-10-09 20:55:33 -0400

    instantiate new player in table constructor.

--- src/com/company/rummy/Table.java
+++ src/com/company/rummy/Table.java
@@ -1,6 +1,7 @@
 package com.company.rummy;
 
 
+import com.company.actor.Player;
 import com.company.deck.Deck;
 import com.company.deck.StandardDeck;
 
@@ -10,6 +11,16 @@ import java.util.List;
 public class Table {
     private List<Hand> hands = new ArrayList<>();
     private Deck deck = new StandardDeck();
+    private int playerCount = 0;
+
+    public Table() {
+//        playerCount = Console.getInt();
+        for (int count = 0; count < playerCount; count++) {
+            Player newPlayer = new Player("Player" + count + 1 + ": ");
+            hands.add(new Hand(newPlayer));
+
+        }
+    }
 
     public void deal() {
         for (int count = 0; count < 10; count++) {
